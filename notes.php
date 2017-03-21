db66c7d0bde32c8f159a16eb05baec1ec75f0da7
lazylionca <me@royh.ca>
2017-03-20 18:09:46 -0600

    Update notes.php

--- notes.php
+++ notes.php
@@ -1,59 +1,60 @@
 <!--
 Nov 5, 2016
 
-
 https://github.com/thephpleague/oauth2-client
-
 https://github.com/thephpleague/oauth2-client/archive/master.zip
 
-There are two terms that can be confusing when deal with oauth:
+There are two terms that can be confusing when dealing with oauth:
 
-User and Client
+        User and Client
 
 The User is the person sitting at the computer.
 That person will have an account with google: ie: sonso@gmail.com
 
-The Client is the program or app that you, the programmer, are writing.
-In order for the Client to query the User's google account,
-the Client must also have a google account, albeit a different type.
+The Client is the program or App that You, the Programmer, are writing.
+In order for the Client/App to query a User's google account,
+the Client/App must also have a Google account, albeit a different type: a service account.
 
-In order for the app/client to get such an account, You the programmer must
-also have a google account: ie memyselni@gmail.com
+In order for an App/Client to get such an account, You the Programmer must
+also have a Google account: ie memyselni@gmail.com
 
-The programmer will need to register at https://console.developers.google.com and 
-create a project and credentials for the app, thus creating a service account for the app.
-You then download a json file which will have a client_id and client_secret that the app needs.
+The Programmer will need to register at https://console.developers.google.com and 
+create a project and credentials, thus creating a service account for the App.
+You then download a json file which will have a client_id and client_secret that the app will use.
 
-Store these securely as you dont want other people to act as if they are your app.
+Store this file securely as you dont want other people to act as if they are your App.
+(That would be bad, mmmkay)
 
-These need to be submitted when your app asks the user to log in to
-their google account and authorize the app to access whatever is
-specified by the app in the 'scope'.
+These credentials need to be submitted whenever your App asks a User to log in to their Google 
+account and authorize the App to access whatever is specified by the App in the 'Scope'.
 
-Once the user has authorized the app by logging into their gmail account,
-the app will receive a 'code'. This code can then be submitted along with the client_id
-to get an access_token and a refresh_token.
 
-This access_token and the app's client_id need to be submitted as part of any query made to the users account.
+Pro-tip: Don't forget to the specify the Scope! (Google it. Go ahead. I'll wait.)
 
-The access token will expire after an hour.
+
+Once the User has authorized the App by logging into their Google account, the App will receive a 'code'. 
+This code can then be submitted along with the App's client_id to get an access_token and a refresh_token.
+
+This access_token and the App's client_id need to be submitted as part of any query made to the User's account.
+
+The access_token will expire after an hour.
 The app then needs to submit it's client_id and the refresh_token in order to get a new access_token.
+goto 40
 
+To paraphrase the Google documentation, your App, once authorized by the User,
+will 'impersonate' said User in order to access information from the User's Google account.
+This is possible because the User has authorized the App's service acount to do so.
 
-To paraphrese the Google documentation, your app, once authorized by the user,
-will 'impersonate' the user in order to access information from their Google account.
-This is possible because the user has authorized the app's service acount to do so.
-
-In short, this is a convoluted, but secure way for a user to give your app
+In short, this is a convoluted, but secure way for a User to give your App
 it's own username and password to their account.
 
 
-The mistake I made in the beginning was assuming that the app's service account
-needed to be created in the developer console under the user account,
-since in my case it's my own account that I'm trying to access.
+The mistake I made in the beginning was assuming that my App's service account
+needed to be created in the developer console under my User account,
+since in my case it's my own account that I'm trying to get my app to access.
 
-It can be done that way, and I did, but it didn't need to be. I'm not changing it now.
-
+It can be done that way, and I did it, but it didn't need to be. 
+I'm not changing it now.
 
 
 //-->
