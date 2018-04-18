cbfdbba16a6c908784720e8b0c1632f7cb399a65
lazylionca <me@royh.ca>
2018-04-18 09:17:54 -0600

    Update introduction.php
    Fixed typo's

--- introduction.php
+++ introduction.php
@@ -4,14 +4,14 @@ Mar 20, 2017
 
 Look up a phone number in Google Contacts.
 
-If you're anythning like me, you want to know how things work rather than just blindly use someone else's library.
+If you're anything like me, you want to know how things work rather than just blindly use someone else's library.
 
-I'll admit, I don't know how to build a combustion engine, but I do know know how to change my own oil and spark plugs.
+I'll admit, I don't know how to build a combustion engine, but I do know how to change my own oil and spark plugs.
 
 Libraries are great when you are working on big projects and don't want to re-invent the wheel.
-But I'm not building a car.
+But I'm not building a car here.
 
-My goal was simple, look up a phone number in my Google Contacts and get the associated name if it existed.
+My goal was simple, look up a phone number in my Google Contacts and get the associated name if it exists.
 This is something that is actually quite simple to do once you figure it out.
 
 Working with api's really isn't that hard.
@@ -23,7 +23,7 @@ the sum total of which required more disk space than my entire project! BLEH!!!
 
 The thing that caught me up in the Google documentation is that all the examples seem to think we as dev's 
 want the users to authorize our app to access something from the users account. 
-Which, yes, is a very common usage, but...
+Which, yes, is probably the most common usage, but...
 
 Apparently the idea of creating an app to access information from your own account is completely unheard of.
 
@@ -40,8 +40,7 @@ https://myaccount.google.com/permissions
 
 Now to make things more confusing, your app also needs to have it's own credentials.
 Register your app as a 'project' on Google Console: https://console.developers.google.com
-if you haven't already. This is how Google knows who/what is accessing a users account.
-
+if you haven't already. This is how Google tracks who/what is accessing a users account.
 
 
 
